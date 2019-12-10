c354bccd563c4069cfe72a3204db820b0aaf8f6c
yhashimoto002 <yhashimoto002@gmail.com>
2019-12-10 13:33:10 +0900

    初コミット

---
+++ powershell/compareFiles/Compare-OfficeFile.ps1
@@ -0,0 +1,415 @@
+﻿param(
+    [parameter(mandatory)]
+    [string]$beforeDir,
+    [parameter(mandatory)]
+    [string]$afterDir
+)
+
+
+function Convert-WordToPdf
+{
+    param(
+        [parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [Alias('FullName')]
+        [string]$Path,
+        [parameter()]
+        [string]$OutDir
+    )
+
+    begin
+    {
+        #Write-Host ("{0:yyyy/MM/dd HH:mm:ss.fff} START converting Word to PDF" -f (Get-Date))
+        #Write-Host ("{0:yyyy/MM/dd HH:mm:ss.fff} ------------------------------" -f (Get-Date))
+    }
+
+    process
+    {
+        $wordFilePath = [string](Resolve-Path $Path)
+        $outDirFullPath = [string](Resolve-Path (Split-Path -Parent $Path))
+        if ($OutDir)
+        {
+            $outDirFullPath = [string](Resolve-Path $outDir)
+        }
+        $pdfFilePath = [string]((Join-Path $outDirFullPath (Split-Path -Leaf $Path)) + ".pdf")
+        $result = ""
+        $errMessage = ""
+
+        try
+        {
+            $wordApplication = New-Object -ComObject Word.Application
+            $wordApplication.Visible = $false
+
+            Write-Host ("{0:yyyy/MM/dd HH:mm:ss.fff} opening {1} ..." -f (Get-Date), $wordFilePath)
+            # https://docs.microsoft.com/ja-jp/dotnet/api/microsoft.office.interop.word.documents.opennorepairdialog?view=word-pia
+            $documents = $wordApplication.Documents.OpenNoRepairDialog($wordFilePath, #FileName
+                                                                        $false,       #ConfirmConversions
+                                                                        $true,        #ReadOnly
+                                                                        $false,       #AddToRecentFiles
+                                                                        "xxxxxx")     #PasswordDocument
+           
+            Write-Host ("{0:yyyy/MM/dd HH:mm:ss.fff} converting {1} to Pdf ..." -f (Get-Date), $wordFilePath)
+            # https://docs.microsoft.com/ja-jp/dotnet/api/microsoft.office.interop.word._document.exportasfixedformat?view=word-pia
+            $documents.ExportAsFixedFormat($pdfFilePath, [Microsoft.Office.Interop.Word.WdExportFormat]::wdExportFormatPDF)
+            Write-Host ("{0:yyyy/MM/dd HH:mm:ss.fff} {1} is successfully converted to PDF." -f (Get-Date), $wordFilePath)
+            $result = "OK"
+        }
+        catch
+        {
+            #Write-Error ("Error: {0}" -f $_.Exception.Message)
+            if ($_.Exception.Message -match "パスワードが正しくありません")
+            {
+                $errMessage = "パスワード保護"
+            }
+            elseif ($_.Exception.Message -match "ファイルが壊れている可能性があります")
+            {
+                $errMessage = "ファイル破損"
+            }
+            elseif ($_.Exception.Message -match "ファイル形式がファイル拡張子と一致していない")
+            {
+                $errMessage = "拡張子不一致"
+            }
+            else
+            {
+                $errMessage = "Error: {0}" -f $_.Exception.Message
+            }
+            $result = "NG"
+            Write-Host ("{0:yyyy/MM/dd HH:mm:ss.fff} {1} is failed to convert. ({2})" -f (Get-Date), $wordFilePath, $errMessage)
+        }
+        finally
+        {
+            # closing
+            #if ($documents) { $documents.Close() }
+            # https://docs.microsoft.com/ja-jp/dotnet/api/microsoft.office.interop.word.documents.close?view=word-pia
+            if ($documents) { $documents.Close([Microsoft.Office.Interop.Word.WdSaveOptions]::wdDoNotSaveChanges) }
+            $wordApplication.Quit()
+            $documents = $wordApplication = $null
+            [GC]::Collect()
+
+            # export to csv
+            $arrayResult = @()
+            $objectOfEachRecord = [pscustomobject]@{
+                FileName=$wordFilePath
+                Result=$result
+                Error=$errMessage
+            }
+            $arrayResult += $objectOfEachRecord
+            $arrayResult | Export-Csv $outFilePathOfConvertOffice  -encoding Default -NoTypeInformation -Append
+            Write-Host ""
+        }
+    }
+
+    end
+    {
+        #Write-Host ("{0:yyyy/MM/dd HH:mm:ss.fff} FINISHED converting Word to PDF" -f (Get-Date))
+        #Write-Host ("{0:yyyy/MM/dd HH:mm:ss.fff} ------------------------------" -f (Get-Date))
+    }
+
+}
+
+
+function Convert-ExcelToPdf
+{
+    param(
+        [parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [Alias('FullName')]
+        [string]$Path,
+        [parameter()]
+        [string]$OutDir
+    )
+
+    begin
+    {
+        #Write-Host ("{0:yyyy/MM/dd HH:mm:ss.fff} START converting Word to PDF" -f (Get-Date))
+        #Write-Host ("{0:yyyy/MM/dd HH:mm:ss.fff} ------------------------------" -f (Get-Date))
+    }
+
+    process
+    {
+        $excelFilePath = [string](Resolve-Path $Path)
+        $outDirFullPath = [string](Resolve-Path (Split-Path -Parent $Path))
+        if ($OutDir)
+        {
+            $outDirFullPath = [string](Resolve-Path $outDir)
+        }
+        $pdfFilePath = [string]((Join-Path $outDirFullPath (Split-Path -Leaf $Path)) + ".pdf")
+        $result = ""
+        $errMessage = ""
+
+        try
+        {
+            $excelApplication = New-Object -ComObject Excel.Application
+            $excelApplication.Visible = $false
+
+            #$workbooks = $excelApplication.Workbooks.OpenNoRepairDialog($excelFilePath)
+            # https://docs.microsoft.com/ja-jp/office/vba/api/excel.workbooks.open
+            Write-Host ("{0:yyyy/MM/dd HH:mm:ss.fff} opening {1} ..." -f (Get-Date), $excelFilePath)
+            $workbooks = $excelApplication.Workbooks.Open($excelFilePath,    #FileName
+                                                            $false,          #UpdateLinks
+                                                            $true,           #ReadOnly
+                                                            [Type]::Missing, #Format
+                                                            "xxxxx")         #Password
+
+            Write-Host ("{0:yyyy/MM/dd HH:mm:ss.fff} converting {1} to PDF ..." -f (Get-Date), $excelFilePath)
+            # https://docs.microsoft.com/ja-jp/dotnet/api/microsoft.office.tools.excel.worksheet.exportasfixedformat?view=vsto-2017
+            $workbooks.ExportAsFixedFormat([Microsoft.Office.Interop.Excel.xlFixedFormatType]::xlTypePDF, $pdfFilePath)
+            $workbooks.Saved = $true
+            Write-Host ("{0:yyyy/MM/dd HH:mm:ss.fff} {1} is successfully converted to PDF." -f (Get-Date), $excelFilePath)
+            $result = "OK"
+        }
+        catch
+        {
+            if ($_.Exception.Message -match "入力したパスワードが間違っています")
+            {
+                $errMessage = "パスワード保護"
+            }
+            elseif ($_.Exception.Message -match "ファイルが壊れている可能性があります")
+            {
+                $errMessage = "ファイル破損"
+            }
+            elseif ($_.Exception.Message -match "ファイル形式がファイル拡張子と一致していない")
+            {
+                $errMessage = "拡張子不一致"
+            }
+            else
+            {
+                $errMessage = "Error: {0}" -f $_.Exception.Message
+            }
+            $result = "NG"
+            Write-Host ("{0:yyyy/MM/dd HH:mm:ss.fff} {1} is failed to convert. ({2})" -f (Get-Date), $excelFilePath, $errMessage)
+        }
+        finally
+        {
+            # closing
+            if ($workbooks) { $workbooks.Close($false) }
+            $excelApplication.Quit()
+            $workbooks = $excelApplication = $null
+            [GC]::Collect()
+
+            # export to csv
+            $arrayResult = @()
+            $objectOfEachRecord = [pscustomobject]@{
+                FileName=$excelFilePath
+                Result=$result
+                Error=$errMessage
+            }
+            $arrayResult += $objectOfEachRecord
+            $arrayResult | Export-Csv $outFilePathOfConvertOffice  -encoding Default -NoTypeInformation -Append
+            Write-Host ""
+        }
+    }
+
+    end
+    {
+        #Write-Host ("{0:yyyy/MM/dd HH:mm:ss.fff} FINISHED converting Word to PDF" -f (Get-Date))
+        #Write-Host ("{0:yyyy/MM/dd HH:mm:ss.fff} ------------------------------" -f (Get-Date))
+    }
+
+}
+
+
+function Convert-PowerPointToPng
+{
+    param(
+        [parameter(Mandatory)]
+        [string]$Path,
+        [parameter(Mandatory)]
+        [string]$OutDir
+    )
+
+    begin
+    {
+        #Write-Host ("{0:yyyy/MM/dd HH:mm:ss.fff} START converting PowerPoint to PDF" -f (Get-Date))
+        #Write-Host ("{0:yyyy/MM/dd HH:mm:ss.fff} ------------------------------" -f (Get-Date))
+    }
+
+    process
+    {
+        $powerpointFullPath = Resolve-Path $Path
+        if (-not (Test-Path $OutDir)) { mkdir $OutDir -Force | Out-Null }
+        try
+        {
+            try
+            {
+                $powerpointApplication = New-Object -ComObject PowerPoint.Application
+            }
+            catch
+            {
+                Write-Host ("cannot create com object: {0}" -f $_.Exception)
+            }
+            #$powerpointApplication.Visible = [Microsoft.Office.Core.MsoTriState]::msoTrue
+
+            Write-Host ("{0:yyyy/MM/dd HH:mm:ss.fff} opening {1} ..." -f (Get-Date), $powerpointFullPath)
+            # https://docs.microsoft.com/ja-jp/previous-versions/office/developer/office-2010/ff763759%28v%3doffice.14%29
+            $password = "xxxxx"
+            $presentations = $powerpointApplication.Presentations.Open([string]$powerpointFullPath+"::$password",
+                                                                        [Microsoft.Office.Core.MsoTriState]::msoTrue,  # readonly
+                                                                        [Type]::Missing,                               # untitled
+                                                                        [Microsoft.Office.Core.MsoTriState]::msoFalse) # withwindow
+            
+            Write-Host ("{0:yyyy/MM/dd HH:mm:ss.fff} converting {1} to PNG ..." -f (Get-Date), $powerpointFullPath)
+            # https://docs.microsoft.com/en-us/previous-versions/office/developer/office-2010/ff762466%28v%3doffice.14%29
+            $presentations.SaveAs($OutDir, [Microsoft.Office.Interop.PowerPoint.PpSaveAsFileType]::ppSaveAsPNG)
+            $presentations.Saved = $true
+            Write-Host ("{0:yyyy/MM/dd HH:mm:ss.fff} {1} is successfully converted to PNG." -f (Get-Date), $powerpointFullPath)
+        }
+        catch
+        {
+            if ($_.Exception.Message -match "読み取りパスワードをもう一度入力してください")
+            {
+                $errMessage = "パスワード保護"
+            }
+            elseif ($_.Exception.Message -match "HRESULT")
+            {
+                $errMessage = "ファイル破損"
+            }
+            else
+            {
+                $errMessage = "Error: {0}" -f $_.Exception.Message
+            }
+            Write-Host ("{0:yyyy/MM/dd HH:mm:ss.fff} {1} is failed to convert. ({2})" -f (Get-Date), $powerpointFullPath, $errMessage)
+        }
+        finally
+        {
+            # closing
+            if ($presentations) { $presentations.Close() }
+            if ($powerpointApplication) { $powerpointApplication.Quit() }
+            $presentations = $powerpointApplication = $null
+            [void][GC]::Collect
+            [void][GC]::WaitForPendingFinalizers()
+            [void][GC]::Collect
+
+            # export to csv
+            $arrayResult = @()
+            $objectOfEachRecord = [pscustomobject]@{
+                FileName=$powerpointFullPath
+                Result=$result
+                Error=$errMessage
+            }
+            $arrayResult += $objectOfEachRecord
+            $arrayResult | Export-Csv $outFilePathOfConvertOffice  -encoding Default -NoTypeInformation -Append
+            Write-Host ""
+        }
+    }
+
+    end
+    {
+        #Write-Host ("{0:yyyy/MM/dd HH:mm:ss.fff} FINISHED converting PowerPoint to PDF" -f (Get-Date))
+        #Write-Host ("{0:yyyy/MM/dd HH:mm:ss.fff} ------------------------------" -f (Get-Date))
+    }
+}
+
+
+function Compare-PowerPoint
+{
+    param(
+        [parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [Alias('FullName')]
+        [string]$PowerPoint
+    )
+
+    process
+    {
+        # skip if target PDF doesn't exist in the opposite dir
+        if (! (Test-Path (Join-Path $afterDir $PowerPoint))) { return }
+        
+        $before_dir = Join-Path $outputDir $PowerPoint | Join-Path -ChildPath "before"
+        $after_dir = Join-Path $outputDir $PowerPoint | Join-Path -ChildPath "after"
+        $diff_dir = Join-Path $outputDir $PowerPoint | Join-Path -ChildPath "diff"
+        mkdir $diff_dir -Force | Out-Null
+
+        # convert pdf to image
+        Write-Host ("{0}" -f ++$count)
+        Convert-PowerPointToPng -Path (Join-Path $beforeDir $PowerPoint) -OutDir $before_dir
+        Convert-PowerPointToPng -Path (Join-Path $afterDir $PowerPoint) -OutDir $after_dir
+
+        # compare images and analyze the difference
+        $arrayResult = @()
+        $page = 0
+        Get-ChildItem $before_dir | Sort-Object -Property LastWriteTime | ForEach-Object {
+            $png = $_.Name
+            magick composite -quiet -compose difference (Join-Path $before_dir $png) `
+                (Join-Path $after_dir $png) (Join-Path $diff_dir $png)
+            [float]$identify = magick identify -format "%[mean]" (Join-Path $diff_dir $png)
+            
+            # output result to csv
+            $result = "NG"
+            $imageBeforePath = Join-Path $before_dir $png
+            $imageAfterPath = Join-Path $after_dir $png
+            $imageDiffPath = Join-Path $diff_dir $png
+            if ($identify -lt $identifyThreshold)
+            {
+                $result = "OK"
+                $imageBeforePath = ""
+                $imageAfterPath = ""
+                $imageDiffPath = ""
+            }
+            Write-Host ("{0}/{1}: {2}({3})" -f $PowerPoint, $png, $result, $identify) 
+            $objectOfEachRecord = [pscustomobject]@{
+                "No."=$count
+                FileName=$PowerPoint
+                ImageName=$png
+                Page=++$page
+                Identify=$identify
+                Result=$result
+                "Image(diff)"=$imageDiffPath
+                "Image(before)"=$imageBeforePath
+                "Image(after)"=$imageAfterPath
+            }
+            $arrayResult += $objectOfEachRecord
+        }
+        $arrayResult | Export-Csv $outCsvFilePath  -Encoding UTF8 -NoTypeInformation -Append
+        Write-Host "------------------------------"
+    }
+}
+
+
+# main
+$startTime = Get-Date
+
+$outFilePathOfConvertOffice = Join-Path $PSScriptRoot ("result_convert_office_" + (Get-Date -Format "yyyy-MM-dd_HHmmss") + ".csv")
+if (Test-Path $outFilePathOfConvertOffice)
+{
+    try
+    {
+        Remove-Item $outFilePath -ErrorAction Stop
+    }
+    catch
+    {
+        Write-Error ("Error: {0}" -f $_.Exception.Message)
+        exit 1
+    }
+}
+
+
+$docRegex = "^.*`.(doc|docx|docm|dot|dotx|dotm)$"
+$excelRegex = "^.*`.(xls|xlsx|xlsm|xlt|xltx|xltm)$"
+$powerpointRegex = "^.*`.(ppt|pptx|pptm|pot|potx|potm|pps|ppsx|ppsm)$"
+#Get-ChildItem $beforeDir | ? { $_.FullName -match $docRegex } | Convert-WordToPdf
+#Get-ChildItem $beforeDir | ? { $_.FullName -match $excelRegex } | Convert-ExcelToPdf
+#Get-ChildItem $afterDir | ? { $_.FullName -match $docRegex } | Convert-WordToPdf
+#Get-ChildItem $afterDir | ? { $_.FullName -match $excelRegex } | Convert-ExcelToPdf
+#Get-ChildItem $beforeDir | ? { $_.Name -match $powerpointRegex } | Compare-PowerPoint
+
+
+foreach($file in Get-ChildItem $beforeDir)
+{
+    if($file.FullName -match $docRegex) { Convert-WordToPdf $file.FullName }
+    if($file.FullName -match $excelRegex) { Convert-ExcelToPdf $file.FullName }
+    if($file.FullName -match $powerpointRegex) { Compare-PowerPoint $file.FullName }
+}
+
+foreach($file in Get-ChildItem $afterDir)
+{
+    if($file.FullName -match $docRegex) { Convert-WordToPdf $file.FullName }
+    if($file.FullName -match $excelRegex) { Convert-ExcelToPdf $file.FullName }
+    if($file.FullName -match $powerpointRegex) { Compare-PowerPoint $file.FullName }
+}
+
+# compare images and analyze the difference
+. ".\Compare-Pdf.ps1" -beforeDir $beforeDir -afterDir $afterDir
+
+$endTime = Get-Date
+Write-Host ("Start: {0}" -f $startTime)
+Write-Host ("End: {0}" -f $endTime)
+Write-Host ("Total: {0}" -f ($endTime - $startTime))
+
---
+++ powershell/modify-configws/Modify-ConfigWS.ps1
@@ -0,0 +1,262 @@
+﻿<#
+.SYNOPSIS
+
+Modify configuration of WS.
+If you omit the MGMT hostname, setup proceed without MGMT (using XML).
+
+.EXAMPLE
+
+PS> .\Modify-ConfigWS.ps1 {WS IPaddress} [{MGMT Hostname}]
+PS> .\Modify-ConfigWS.ps1 10.12.176.132 YUHASHIMOTO030 
+PS> .\Modify-ConfigWS.ps1 10.12.176.132 
+
+#>
+
+param (
+    [Parameter(Position=0, Mandatory=$true)]
+    [string] $IpAddr,
+    [Parameter(Position=1, Mandatory=$false)]
+    [string] $Hostname
+)
+
+
+# check parameter
+if (! ($IpAddr -match "\d+\.\d+\.\d+\.\d+"))
+{
+    Write-Host "Please set IP address of WS"
+    exit 1
+}
+
+
+# do not change
+$ws_install_dir = "\\$IpAddr\c$\Program Files\Votiro\SDS Web Service"
+$fc_install_dir = "\\$IpAddr\c$\Program Files\Votiro\Votiro File Connector"
+$ws_config_machine = "$ws_install_dir\config\machine.xml"
+$ws_config_webapi = "$ws_install_dir\config\webapi.xml"
+$ws_config_publish = "$ws_install_dir\Policy\publish.xml"
+$ws_config_websrv = "$ws_install_dir\Policy\sanitize_websrv.xml"
+$fc_config = "$fc_install_dir\FileConnector.config"
+
+
+# load config
+$conf = Get-Content (Join-Path $PSScriptRoot "settings.ini") | ? { $_ -match "=" } | ConvertFrom-StringData
+$fe_config = $conf.FeConfig
+$default_channel_in = $conf.DefaultChannelIn
+$default_channel_out = $conf.DefaultChannelOut
+$channel_name = $conf.ChannelName
+$channel_in = $conf.ChannelIn
+$channel_out = $conf.ChannelOut
+$policy_config_name = $conf.PolicyConfigurationName
+$policy_name = $conf.PolicyName
+
+$simple_policy = @{}
+$simple_policy.Add("CleanOffice", $conf.CleanOffice)
+$simple_policy.Add("CleanPdf", $conf.CleanPdf)
+$simple_policy.Add("CleanImages", $conf.CleanImages)
+$simple_policy.Add("CleanCad", $conf.CleanCad)
+$simple_policy.Add("ExtractEmls", $conf.ExtractEmls)
+$simple_policy.Add("BlockPasswordProtectedArchives", $conf.BlockPasswordProtectedArchives)
+$simple_policy.Add("BlockPasswordProtectedOffice", $conf.BlockPasswordProtectedOffice)
+$simple_policy.Add("BlockPasswordProtectedPdfs", $conf.BlockPasswordProtectedPdfs)
+$simple_policy.Add("BlockAllPasswordProtected", $conf.BlockAllPasswordProtected)
+$simple_policy.Add("Blockunsupported", $conf.Blockunsupported)
+$simple_policy.Add("ScanVirus", $conf.ScanVirus)
+$simple_policy.Add("BlockUnknownFiles", $conf.BlockUnknownFiles)
+$simple_policy.Add("ExtractArchiveFiles", $conf.ExtractArchiveFiles)
+$simple_policy.Add("BlockEquationOleObject", $conf.BlockEquationOleObject)
+$simple_policy.Add("BlockBinaryFiles", $conf.BlockBinaryFiles)
+$simple_policy.Add("BlockScriptFiles", $conf.BlockScriptFiles)
+$simple_policy.Add("BlockFakeFiles", $conf.BlockFakeFiles)
+
+
+# backup
+if(! (Test-Path $ws_config_machine".org"))
+{
+    cp $ws_config_machine -Destination $ws_config_machine".org"
+}
+if(! (Test-Path $ws_config_webapi".org"))
+{
+    cp $ws_config_webapi -Destination $ws_config_webapi".org"
+}
+if(! (Test-Path $ws_config_publish".org"))
+{
+    cp $ws_config_publish -Destination $ws_config_publish".org"
+}
+if(! (Test-Path $ws_config_websrv".org"))
+{
+    cp $ws_config_websrv -Destination $ws_config_websrv".org"
+}
+if(! (Test-Path $fc_config".org"))
+{
+    cp $fc_config -Destination $fc_config".org"
+}
+
+
+# parse XML
+$ws_xml_machine = [xml](Get-Content $ws_config_machine)
+$ws_xml_webapi = [xml](Get-Content $ws_config_webapi)
+$ws_xml_publish = [xml](Get-Content $ws_config_publish)
+$fc_xml = [xml](Get-Content $fc_config)
+
+
+# modify WS and FC config
+# if MGMT hostname is not given, XML is used instead
+Write-Host "Modifying WS configuration ..."
+
+# FileConnector.config
+$fc_xml.FileConnector.MaxWebApiThreads = "64"
+$fc_xml.FileConnector.WebApiSettings.WebApiTimeOutInMS = "3600000"
+$fc_xml.FileConnector.WebApiSettings.WebApiStatusCheckIntervalInMS = "1000"
+($fc_xml.FileConnector.Channels.Channel | ? { $_.Name -eq "DefaultChannel" }).In = $default_channel_in
+($fc_xml.FileConnector.Channels.Channel | ? { $_.Name -eq "DefaultChannel" }).Out = $default_channel_out
+($fc_xml.FileConnector.Channels.Channel | ? { $_.Name -eq "DefaultChannel" }).InProcessItemsLimit = "150"
+$node = $fc_xml.FileConnector.Channels
+$childnode = $node.Channel | ? { $_.Name -eq $channel_name}
+if (! $childnode)
+{
+    $element_channel = $fc_xml.CreateElement("Channel")
+    [void]$element_channel.SetAttribute("Name", $channel_name)
+    [void]$element_channel.SetAttribute("In", $channel_in)
+    [void]$element_channel.SetAttribute("Out", $channel_out)
+    [void]$element_channel.SetAttribute("PolicyConfigurationName", $policy_config_name)
+    [void]$element_channel.SetAttribute("IgnoreEmptyFile", "true")
+    [void]$element_channel.SetAttribute("DeleteAfterSanitization", "true")
+    [void]$element_channel.SetAttribute("InProcessItemsLimit", "150")
+    [void]$element_channel.SetAttribute("FileNotChangedInSeconds", "2")
+    [void]$element_channel.SetAttribute("ExcludedExtensions", ".partial,.part,.crdownload")
+    [void]$element_channel.SetAttribute("IgnoreFilesWithoutExtension", "False")
+    [void]$element_channel.SetAttribute("MaxFileSizeInBytes", "9223372036854775807")
+    [void]$node.AppendChild($element_channel)
+}
+
+$node = $fc_xml.FileConnector.Policies
+$childnode = $node.PolicyParams | ? { $_.Name -eq $policy_config_name}
+if (! $childnode)
+{
+    $element_policyparams = $fc_xml.CreateElement("PolicyParams")
+    [void]$element_policyparams.SetAttribute("Name", $policy_config_name)
+    [void]$node.AppendChild($element_policyparams)
+    $childnode = $node.PolicyParams | ? { $_.Name -eq $policy_config_name}
+    $element_predefinedpolicy = $fc_xml.CreateElement("PredefinedPolicy")
+    [void]$element_predefinedpolicy.SetAttribute("PolicyName",$policy_name)
+    [void]$childnode.AppendChild($element_predefinedpolicy)
+    $element_policyrules = $fc_xml.CreateElement("PolicyRules")
+    [Xml.XmlNode]$childchildnode = $childnode.AppendChild($element_policyrules)
+    foreach ($entry in $simple_policy.GetEnumerator())
+    {
+        $element_add = $fc_xml.CreateElement("add")
+        [void]$element_add.SetAttribute("Name", $entry.Key)
+        [void]$element_add.SetAttribute("Value", $entry.Value)
+        [void]$childchildnode.AppendChild($element_add)
+    }
+}
+
+if ($Hostname)
+{
+    # machine.xml
+    $node = $ws_xml_machine.VotiroConfiguration.MachineSettings
+    if (! $node.ManagementSettings)
+    {
+        $element_managementsettings = $ws_xml_machine.CreateElement("ManagementSettings")
+        [void]$element_managementsettings.SetAttribute("SanitizationInfoServerUrl", "https://${hostname}:7070")
+        if ($node.SandboxServiceSettings) # v8.3 or above
+        {
+            [void]$element_managementsettings.SetAttribute("BlobManagerServerUrl", "https://${hostname}:3030")
+        }
+        else
+        {
+            [void]$element_managementsettings.SetAttribute("BlobManagerServerUrl", "http://${hostname}:3030")
+        }
+        [void]$element_managementsettings.SetAttribute("AuthToken", "30acc6eb-16d9-4133-ae43-0f5b6d40a318")
+        [void]$node.AppendChild($element_managementsettings)
+    }
+    else
+    {
+        $ws_xml_machine.VotiroConfiguration.MachineSettings.ManagementSettings.SanitizationInfoServerUrl = "https://${hostname}:7070"
+        if ($node.SandboxServiceSettings) # v8.3 or above
+        {
+            $ws_xml_machine.VotiroConfiguration.MachineSettings.ManagementSettings.BlobManagerServerUrl = "https://${hostname}:3030"
+        }
+        else
+        {
+            $ws_xml_machine.VotiroConfiguration.MachineSettings.ManagementSettings.BlobManagerServerUrl = "http://${hostname}:3030"
+        }
+        $ws_xml_machine.VotiroConfiguration.MachineSettings.ManagementSettings.AuthToken = "30acc6eb-16d9-4133-ae43-0f5b6d40a318"
+    }
+
+    # webapi.xml
+    $ws_xml_webapi.WebApiServerConfig.NamedPolicyFolderPath = ""
+    $ws_xml_webapi.WebApiServerConfig.NamedPolicyServerUri = "https://${hostname}:7070"
+    $ws_xml_webapi.WebApiServerConfig.AuthToken = "30acc6eb-16d9-4133-ae43-0f5b6d40a318"
+
+    # publish.xml
+    $node = $ws_xml_publish.ArrayOfPolicyRule.PolicyRule.Actions
+    $childnode = $node.PolicyAction | ? { $_.type -eq "ReportToManagementAction"}
+    if(! $childnode)
+    {
+        $element_policyaction = $ws_xml_publish.CreateElement("PolicyAction")
+        [void]$element_policyaction.SetAttribute("type", $ws_xml_publish.ArrayOfPolicyRule.xsi, "ReportToManagementAction")
+        $element_backupfolderpath = $ws_xml_publish.CreateElement("BackupFolderPath")
+        $element_backupfolderpath.InnerText = "C:\backup"
+        [void]$node.AppendChild($element_policyaction)
+        $childnode = $node.PolicyAction | ? { $_.type -eq "ReportToManagementAction"}
+        [void]$childnode.AppendChild($element_backupfolderpath)
+    }
+
+    # sanitize_websrv.xml
+    cp $ws_config_websrv".org" -Destination $ws_config_websrv -Force
+
+    # FileConnector.config
+    $node = $fc_xml.FileConnector.Policies
+    $childnode = $node.PolicyParams | ? { $_.Name -eq "DefaultPolicy"}
+    $childnode.PredefinedPolicy.PolicyName = "Default Policy"
+    $childnode = $node.PolicyParams | ? { $_.Name -eq $policy_config_name}
+    $childnode.PredefinedPolicy.PolicyName = $policy_name
+}
+else
+{
+    # machine.xml
+    $node = $ws_xml_machine.VotiroConfiguration.MachineSettings
+    if ($node.ManagementSettings)
+    {
+        [void]$node.RemoveChild($node.ManagementSettings)
+    }
+    
+    # webapi.xml
+    $ws_xml_webapi.WebApiServerConfig.NamedPolicyFolderPath = "Policy"
+    $ws_xml_webapi.WebApiServerConfig.NamedPolicyServerUri = ""
+    $ws_xml_webapi.WebApiServerConfig.AuthToken = ""
+
+    # publish.xml
+    $node = $ws_xml_publish.ArrayOfPolicyRule.PolicyRule.Actions
+    $childnode = $node.PolicyAction | ? { $_.type -eq "ReportToManagementAction"}
+    if ($childnode)
+    {
+        [void]$node.RemoveChild($childnode)
+    }
+
+    # sanitize_websrv.xml
+    cp $fe_config -Destination $ws_config_websrv -Force
+
+    # FileConnector.config
+    $node = $fc_xml.FileConnector.Policies
+    $childnode = $node.PolicyParams | ? { $_.Name -eq "DefaultPolicy"}
+    $childnode.PredefinedPolicy.PolicyName = ""
+    $childnode = $node.PolicyParams | ? { $_.Name -eq $policy_config_name}
+    $childnode.PredefinedPolicy.PolicyName = ""
+}
+
+$ws_xml_machine.Save($ws_config_machine)
+$ws_xml_webapi.Save($ws_config_webapi)
+$fc_xml.Save($fc_config)
+$ws_xml_publish.Save($ws_config_publish)
+
+
+# restart service
+Write-Host "Restarting Service ..."
+Get-Service -Name VotiroSAPI -ComputerName $IpAddr | Restart-Service
+Get-Service -Name VotiroSNMC -ComputerName $IpAddr | Restart-Service
+Get-Service -Name Votiro.FileConnector.WindowsService.exe -ComputerName $IpAddr | Restart-Service
+Get-Service -ComputerName $IpAddr | Where-Object { $_.Name -eq 'VotiroSAPI' }
+Get-Service -ComputerName $IpAddr | Where-Object { $_.Name -eq 'VotiroSNMC' }
+Get-Service -ComputerName $IpAddr | Where-Object { $_.Name -eq 'Votiro.FileConnector.WindowsService.exe' }
