2ff200e77b514b6510667a49f7743b0a38641eca
yhashimoto002 <yhashimoto002@gmail.com>
2020-01-29 15:09:34 +0900

    初コミット

---
+++ powershell/general/Expand-Archive-7zip.ps1
@@ -0,0 +1,52 @@
+﻿function Expand-Archive-7zip
+{
+    param(
+        [parameter(mandatory)][string]$FilePath,
+        [string]$OutDir = $PSScriptRoot
+    )
+
+    $7zexe = $PSScriptRoot + "\7z.exe"
+
+    # パスワードの指定
+    $fileName = Split-Path $FilePath -Leaf
+    $password = ""
+    if ($fileName -eq "license.bin")
+    {
+        $password = "nuchhkyhe"
+    }
+    elseif ($fileName -eq "config.bin")
+    {
+        $password = "vdsru,ngrf,@2@"
+    }
+    elseif (($fileName -eq "actions.bin") -or ($fileName -eq "c-smd.bin"))
+    {
+        $password = "aslktybn"
+    }
+
+    # 7z.exe に渡すパラメータ
+    if ($password)
+    {
+        $arg = "x -y -p$password -o$OutDir $FilePath"
+    }
+    else
+    {
+        $arg = "x -y -o$OutDir $FilePath"
+    }
+
+    # 展開
+    $psi = New-Object System.Diagnostics.ProcessStartInfo
+    $psi.FileName = $7zexe
+    $psi.RedirectStandardError = $true
+    $psi.RedirectStandardOutput = $false
+    $psi.CreateNoWindow = $true
+    $psi.UseShellExecute = $false
+    $psi.Arguments = $arg
+
+    $process = New-Object System.Diagnostics.Process
+    $process.StartInfo = $psi
+    $process.Start() | Out-Null
+    $process.WaitForExit()
+}
+
+
+
---
+++ powershell/general/Invoke-WebrequestToUpdateServer.ps1
@@ -0,0 +1,48 @@
+﻿$curl = Join-Path $PSScriptRoot "curl.exe"
+
+function Invoke-WebrequestToUpdateServer
+{
+    param (
+        [parameter(mandatory)][string]$UserName,
+        [string]$StationName,
+        [string]$Method = "GET",
+        [string]$File,
+        [string]$OutFile
+    )
+
+    $userNameForCred = "asgent"
+    $passwordForCred = "gusestA7"
+    $cred = New-Object PSCredential $userNameForCred, (ConvertTo-SecureString $passwordForCred -AsPlainText -Force)
+    $updateServerUrl = "https://updates.votiro.com/"
+    $fAvSmd = "av-smd.bin"
+    $fAvSmdSig2 = "av-smd.bin.sig2"
+
+    # URL
+    # av-smd.bin (.sig2): /username/xxx.bin(.sig2)
+    # av-smd.bin (.sig2) 以外: /username/station/xxx.bin(.sig2)
+    if (($File -eq $fAvSmd) -Or ($File -eq $fAvSmdSig2))
+    {
+        $url = $updateServerUrl + $UserName + "/" + $File
+    }
+    else
+    {
+        $url = $updateServerUrl + $UserName + "/" + $StationName + "/" + $File
+    }
+
+    # 出力ファイル
+    if (! $outFile)
+    {
+        $outFile = $File
+    }
+ 
+
+    # Web リクエスト
+    if ( $Method -eq "HEAD" )
+    {
+        & $curl -I -sS -f --connect-timeout 5 --user ${userNameForCred}:$passwordForCred $url 2>&1
+    }
+    else
+    {
+        & $curl -f -D - --connect-timeout 5 --user ${userNameForCred}:$passwordForCred -o $outFile $url
+    }
+}
